dedc2822c19b68c62af979f67c1693e95c53118b
krastsislau <krastsislau@gmail.com>
2022-07-24 21:51:14 +0400

    added views for trainmode

--- anki/views.py
+++ anki/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+import random
 
 from django.http import HttpResponse, HttpResponseNotFound
 
@@ -15,6 +16,10 @@ from anki.serializers import (UserSerializer,
                               StatSerializer)
 
 from api.settings import JWT_AUTH
+
+# TODO: add validation for post request dict keys and return
+# "Bad Request" responses if invalid.
+# TODO: check existense of the token holder
 
 
 class GetMe(APIView):
@@ -187,9 +192,9 @@ class UpdateDeckStuff(APIView):
     Update {my} deck's stuff (i.e., name, color, public/private status,
     description, and cards).
 
-    Endpoint: `api/update-deck-stuff`
+    Endpoint: `api/update-deck-stuff?username={username}`
 
-    Input: jwt, deck stuff
+    Input: jwt, username, deck stuff
     1. Process (jwt)user's deck stuff and modify the database objects
     """
     def post(self, request: Request, format: Any = None) -> Response:
@@ -237,19 +242,96 @@ class PullNextCard(APIView):
     Get a not-exactly-random card from a deck.
     To clarify: "not-exactly-random" means the next card that
     the backend find appropriate to give to a user.
+    NOTE: it IS random for now :)
 
     Basically the logic of this view is the biggest pain in the ass
     of the whole app.
+
+    Endpoint: `pull-next-card?deck_owner_username={username}&deckname={deckname}`
+
+    Params: username, deckname, jwt
+    Logic:
+        0. no jwt -> no stuff (401)
+        1. Does the {username} user exist ? continue : 404(user)
+        2. Does the {deckname} deck exist ? continue : 404(deck)
+        3. [tmp] Get random card from the deck
     """
     def get(self, request: Request, format: Any = None) -> Response:
-        pass  # here the app supposedly accesses all the stats on
-              # a deck and decides what card is the most suitable to
-              # return.
+        username = request.query_params.get('deck_owner_username')
+        deckname = request.query_params.get('deckname')
+        # 0:
+        jwt_username = request.user.username
+        if not jwt_username:
+            return HttpResponse(status=401)
+        # 1:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return HttpResponseNotFound(f'{username} user not found')
+        # 2:
+        try:
+            deck = Deck.objects.get(owner=user, name=deckname)
+        except Deck.DoesNotExist:
+            return HttpResponseNotFound(f'{deckname} deck not found')
+        # 3:
+        cards = list(Card.objects.filter(deck=deck))
+        if not cards:
+            return Response('no cards in deck')
+        else:
+            random_card = random.choice(cards)
+            card_serializer = CardSerializer(random_card)
+            return Response(card_serializer.data)
 
 
 class PostFeedback(APIView):
     """
     Post feedback on a card that a user got.
+
+    Endpoint: `post-feedback`
+
+    Input: jwt, deck_owner_username, deckname, card_id, feedback
+    Logic:
+        0: no jwt -> no posting feedback (401)
+        1: Does the jwt user exist ? continue : 401
+        2: Does the {deck_owner_username} user exist ? continue
+                                                     : 404(user)
+        3: Does the {deckname} deck exist ? continue : 404(deck)
+        4: Does the {card_id} card exist ? continue : 404(card)
+        5: Create new Stat(jwt.user, deck) with specified feedback
     """
     def post(self, request: Request, format: Any = None) -> Response:
-        pass
+        deck_owner_username = request.data.get('deck_owner_username')
+        deckname = request.data.get('deckname')
+        card_id = request.data.get('card_id')
+        feedback = request.data.get('feedback')
+        # 0:
+        jwt_username = request.user.username
+        if not jwt_username:
+            return HttpResponse(status=401)
+        # 1:
+        try:
+            jwt_user = User.objects.get(username=jwt_username)
+        except User.DoesNotExist:
+            return HttpResponse(status=401)
+        # 2:
+        try:
+            user = User.objects.get(username=deck_owner_username)
+        except User.DoesNotExist:
+            return HttpResponseNotFound(f'{deck_owner_username} user '
+                                         'not found')
+        # 3:
+        try:
+            deck = Deck.objects.get(owner=user, name=deckname)
+        except Deck.DoesNotExist:
+            return HttpResponseNotFound(f'{deckname} deck not found')
+        # 4:
+        try:
+            card = Card.objects.get(deck=deck, pk=card_id)
+        except Deck.DoesNotExist:
+            return HttpResponseNotFound(f'{deckname} deck not found')
+        # 5:
+        stat = Stat(feedback=feedback,
+                    owner=jwt_user,
+                    card=card)
+        stat.save()
+        return Response()

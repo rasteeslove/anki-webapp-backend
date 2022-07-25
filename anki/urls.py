876811b7aff81edd2072eb56aa5e16ec4b07efa4
krastsislau <krastsislau@gmail.com>
2022-07-25 21:33:45 +0400

    fixed post feedback view issue

--- anki/urls.py
+++ anki/urls.py
@@ -17,5 +17,5 @@ anki_urls = [
     path('get-deck-stuff', GetDeckStuff.as_view()),
     path('update-deck-stuff', UpdateDeckStuff.as_view()),
     path('pull-next-card', PullNextCard.as_view()),
-    path('post-feedback', PostFeedback.as_view()),
+    path('post-feedback/', PostFeedback.as_view()),
 ]
